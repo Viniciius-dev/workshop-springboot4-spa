5d674549f2520bab42b9ee5c44bad64eb24afe2a
Vinícius-Castro <viniciuscasalves654@gmail.com>
2026-08-10 00:59:20 -0300

    Initial commit

---
+++ src/main/java/org/example/api_curso_section22/ApiCursoSection22Application.java
@@ -0,0 +1,13 @@
+package org.example.api_curso_section22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCursoSection22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCursoSection22Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/api_curso_section22/ApiCursoSection22ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.api_curso_section22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursoSection22ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
