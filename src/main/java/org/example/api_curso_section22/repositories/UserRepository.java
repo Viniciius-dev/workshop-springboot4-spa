78f3e151dc2862f6979b87daf6e879d1ba962fe5
Vinícius-Castro <viniciuscasalves654@gmail.com>
2026-08-12 01:23:46 -0300

    Service layer, component registration

--- src/main/java/org/example/api_curso_section22/repositories/UserRepository.java
+++ src/main/java/org/example/api_curso_section22/repositories/UserRepository.java
@@ -2,8 +2,15 @@ package org.example.api_curso_section22.repositories;
 
 import org.example.api_curso_section22.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@Repository
+@RequestMapping(value = "/users")
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
+    List<User> id(Long id);
 }
---
+++ src/main/java/org/example/api_curso_section22/services/UserService.java
@@ -0,0 +1,26 @@
+package org.example.api_curso_section22.services;
+
+import org.example.api_curso_section22.entities.User;
+import org.example.api_curso_section22.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+      Optional<User> obj = userRepository.findById(id);
+      return obj.get();
+    }
+}
